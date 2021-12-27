cf598a56c726e70c0a75a25c0b765fd01a3a350b
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-12-27 03:19:30 -0500

    ¡Quinary Commit!

---
+++ README.md
@@ -0,0 +1,16 @@
+# Is Not Odd Even (TypeScript)
+
+I have observed [Is Odd Even TS](https://www.npmjs.com/package/is-not-odd-even-ts) and I have found it lacking. For this reason, Is Not Odd Even (Typescript) must exist.
+
+## Installation
+
+`npm i https://www.npmjs.com/package/is-not-odd-even-ts`
+
+## Usage
+
+```js
+const INOETS = require("is-not-odd-even-ts"); // JS
+const INOETS = require("is-not-odd-even-ts"); // TS
+
+INOETS.
+```
--- test/test.js
+++ test/test.js
@@ -1,21 +1,21 @@
-const inoets = require("../build/index");
+const INOETS = require("../build/index");
 const assert = require("assert");
 
 describe("Determine whether inputs are odd or even, not odd, or not even", () => {
 
     it("should return false", () => {
-        assert.equal(inoets.isEven(1), false);
+        assert.equal(INOETS.isEven(1), false);
     });
 
     it("should return false", () => {
-        assert.equal(inoets.isOdd(2), false);
+        assert.equal(INOETS.isOdd(2), false);
     });
 
     it("should return true", () => {
-        assert.equal(inoets.isNotEven(1.2), true);
+        assert.equal(INOETS.isNotEven(1.2), true);
     });
 
     it("should return false", () => {
-        assert.equal(inoets.isNotOdd(1), false);
+        assert.equal(INOETS.isNotOdd(1), false);
     });
 });
