a80ad3bfe57040679b5c8b378771c86032cee00b
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-12-27 03:51:49 -0500

    Septenary commit 👎👎👎

--- source/index.ts
+++ source/index.ts
@@ -1,22 +1,6 @@
-export function isEven(input: number): boolean {
-    if ([0, 2, 4, 6, 8].includes(Number(String(input)[String(input).length - 1])) && input == Math.floor(input)) {
-        return true;
-    } else {
-        return false;
-    }
-}
+type Type = (input: number) => boolean;
 
-
-export function isNotEven(input: number): boolean {
-    return !isEven(input);
-}
-
-
-export function isOdd(input: number): boolean {
-    return isNotEven(input) && input == Math.floor(input);
-}
-
-
-export function isNotOdd(input: number): boolean {
-    return !isOdd(input);
-}
+export const isEven:    Type = input => ([0, 2, 4, 6, 8].includes(Number(String(input)[String(input).length - 1])) && input == Math.floor(input)) ? true : false;
+export const isNotEven: Type = input => !isEven(input) ? true : false; 
+export const isOdd:     Type = input => isNotEven(input) && input == Math.floor(input) ? true : false;
+export const isNotOdd:  Type = input => !isOdd(input) ? true : false;
